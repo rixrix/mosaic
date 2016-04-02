e54154c5c103b0fb2286c5be1a85533bc04236c2
Richard Sentino <richard@mingdinative.com>
2016-04-02 16:34:29 +1300

    chore(js): Init work on file load

--- js/client.js
+++ js/client.js
@@ -1,2 +1,33 @@
-// Edit me.
-console.log('client');
+
+document.addEventListener('drop', function(event) {
+    event.preventDefault();
+});
+
+document.addEventListener('dragenter', function(event){
+    if (event.target.className == 'Container-dragDropImage') {
+        event.target.style.border = '2px dotted red';
+    }
+});
+
+document.addEventListener('dragover', function(event){
+    event.preventDefault();
+});
+
+document.addEventListener('drop', function(event) {
+    event.preventDefault();
+    droppedImageHandler(event);
+});
+
+function droppedImageHandler(event) {
+    var file = event.dataTransfer.files[0];
+    var reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = (function() {
+        console.log(reader.result);
+    });
+
+
+    console.log(file);
+}
